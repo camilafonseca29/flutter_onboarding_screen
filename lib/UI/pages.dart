261a6204236aa2dbf7b7f2d2e6c94a7b33009d11
camilafonseca29 <camila.s.fonseca29@gmail.com>
2019-05-19 17:22:12 -0300

    Update pages.dart
    Add ListView

--- lib/UI/pages.dart
+++ lib/UI/pages.dart
@@ -33,57 +33,59 @@ class Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-        width: double.INFINITY,
-        color: viewModel.color,
-        child:
-            new Opacity(
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: viewModel.color),
+        child: ListView(
+          children: <Widget>[
+            Opacity(
               opacity: percentVisible,
-              child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-          new Transform(
-            transform: new Matrix4.translationValues(0.0, 50.0 * (1.0 - percentVisible) ,0.0),
-            child: new Padding(
-                padding: new EdgeInsets.only(bottom: 25.0),
-                child:
-                new Image.asset(
-                    viewModel.heroAssetPath,
-                    width: 200.0,
-                    height: 200.0),
-            ),
-          ),
-          new Transform(
-            transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible) ,0.0),
-            child: new Padding(
-                padding: new EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: new Text(
-                  viewModel.title,
-                  style: new TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'FlamanteRoma',
-                    fontSize: 34.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Transform(
+                    transform: Matrix4.translationValues(
+                        0.0, 50.0 * (1.0 - percentVisible), 0.0),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 50.0, bottom: 50.0),
+                      child: Image.asset(viewModel.heroAssetPath,
+                          width: 200.0, height: 200.0),
+                    ),
                   ),
-                ),
-            ),
-          ),
-          new Transform(
-            transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible) ,0.0),
-            child: new Padding(
-                padding: new EdgeInsets.only(bottom: 75.0),
-                child: new Text(
-                  viewModel.body,
-                  textAlign: TextAlign.center,
-                  style: new TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'FlamanteRomaItalic',
-                    fontSize: 18.0,
+                  Transform(
+                    transform: Matrix4.translationValues(
+                        0.0, 30.0 * (1.0 - percentVisible), 0.0),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Text(
+                        viewModel.title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'FlamanteRoma',
+                          fontSize: 34.0,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-            ),
-          ),
-        ]),
-            ));
+                  Transform(
+                    transform: Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+                    child: Padding(padding: EdgeInsets.only(bottom: 75.0),
+                      child: Text(viewModel.body,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'FlamanteRomaItalic',
+                        fontSize: 18.0,
+                      ),),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
